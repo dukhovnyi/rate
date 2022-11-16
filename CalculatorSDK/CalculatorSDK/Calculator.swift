5eafb394e2a4ba73acb177d3a7c44a25b6815d5a
Yurii Dukhovnyi <y.dukhovnyi@gmail.com>
2022-11-16 18:33:43 +0200

    Create Xcode projects for SDK and App.

---
+++ CalculatorSDK/CalculatorSDK/Calculator.swift
@@ -0,0 +1,10 @@
+//
+//  Calculator.swift
+//  CalculatorSDK
+//
+//  Created by Yurii Dukhovnyi on 16.11.2022.
+//
+
+import Foundation
+
+public enum Calculator {}
---
+++ CalculatorSDK/CalculatorSDK/CalculatorSDK.h
@@ -0,0 +1,18 @@
+//
+//  CalculatorSDK.h
+//  CalculatorSDK
+//
+//  Created by Yurii Dukhovnyi on 16.11.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CalculatorSDK.
+FOUNDATION_EXPORT double CalculatorSDKVersionNumber;
+
+//! Project version string for CalculatorSDK.
+FOUNDATION_EXPORT const unsigned char CalculatorSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalculatorSDK/PublicHeader.h>
+
+
